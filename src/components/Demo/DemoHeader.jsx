0a806ac20eccf32bccfacc2c2cc9d6d9f4e2a930
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-06 02:20:11 +0500

    favicon update, logo, demo header changes made

--- src/components/Demo/DemoHeader.jsx
+++ src/components/Demo/DemoHeader.jsx
@@ -13,12 +13,16 @@ const DemoHeader = () => {
           alt="logo" 
           />
         </Link>
-        <div>
-          <div>
+        <div className='flex items-center gap-5'>
+          <div className='hidden text-sm sm:flex items-center gap-5'>
             {nav.map((link, i) => (
-              <Link to={link.path}>{link.title}</Link>
+              <Link key={i} to={link.path}>{link.title}</Link>
             ))}
           </div>
+          <div className="relative">
+            <button className='hidden text-sm sm:flex items-center gap-5'>Sign In</button>
+          </div>
+          <button className='bg-black text-white rounded-full px-3 p-2 text-sm font-medium'>Get Started</button>
         </div>
       </div>
     </header>
