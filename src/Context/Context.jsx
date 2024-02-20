dc02632683761aeccabb3b7998bac59561e616e9
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-20 23:53:37 +0500

    Global Context created

---
+++ src/Context/Context.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const BlogContext = createContext();
+
+const Context = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(false);
+  return (
+    <div>
+      <BlogContext.Provider value={{ currentUser, setCurrentUser }}>
+        {children}
+      </BlogContext.Provider>
+    </div>
+  )
+}
+
+export default Context
+
+export const Blog = () => useContext(BlogContext);
